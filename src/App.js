6143bc1203e82ff5832ef3ada983c5a69008d1dc
AleksandrTseninNx <cenin70@gmail.com>
2024-11-09 22:40:16 +0300

    initial page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
+import { OrbitProgress } from "react-loading-indicators";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Logo width='50%' height='50%' />
+      <p>Тьма надвигается!</p>
+      <OrbitProgress variant="disc" color="#ffffff" size="medium" speedPlus="-5" text="" textColor="" />
       </header>
+
     </div>
   );
 }
