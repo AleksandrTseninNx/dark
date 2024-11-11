6cd54a17d7ee57cd57f6babe4a45b4fa075741f7
AleksandrTseninNx <cenin70@gmail.com>
2024-11-11 18:31:30 +0300

    add fixed position

--- src/App.js
+++ src/App.js
@@ -188,7 +188,7 @@ function App() {
           </BottomNavigation>
         </Paper> 
       </Flex>
-      <Flex className='WatermarkWrapper'>
+      <Flex position='fixed' className='WatermarkWrapper'>
         <Desinged width='150px' height='24px' />
       </Flex>
     </Flex>
